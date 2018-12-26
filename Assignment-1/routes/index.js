7f3cd9613e972eda7367040c27e7790ab012e0a5
Alejandro Garcia <alejandro.garcia@thomascookonline.com>
2018-12-26 13:05:10 +0100

    Assignment-1 - Refactoring code

--- Assignment-1/routes/index.js
+++ Assignment-1/routes/index.js
@@ -1,14 +1,18 @@
-const handlers = require('../handlers');
+const {
+  hello,
+  methodNotAllowed,
+  notFound
+} = require('../handlers');
 
 const routes = {
   'hello': {
-      post: handlers.hello
+      post: hello
   }
 };
 
 const handlerResolver = (path, method) => {
   const route = routes[path];
-  return route ? route[method] || handlers.methodNotAllowed : handlers.notFound;
+  return route ? route[method] || methodNotAllowed : notFound;
 };
 
 module.exports = ({path, method, ...data}, callback) => {
